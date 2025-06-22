8376324ad8d3ba922172f6d745156c3f54c38987
versitynihal <versitynihal@gmail.com>
2025-06-22 16:13:22 +0000

    another

--- codeP/runner.py
+++ codeP/runner.py
@@ -1,4 +1,4 @@
 def run_pipeline(model_name, dataset_name, options_name=None):
     # This will run in main.py
-    print('hello world')
+    print(model_name, dataset_name, options_name)
     return
