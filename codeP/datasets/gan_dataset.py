0bbd9b86c3bbea80c6680dfa6a72db2e8a01c5ea
LegendNihal <65503207+LegendNihal@users.noreply.github.com>
2025-06-24 23:51:05 +0600

    GAN final

--- codeP/datasets/gan_dataset.py
+++ codeP/datasets/gan_dataset.py
@@ -10,7 +10,7 @@ def load_data():
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
-    dataset = torchvision.datasets.CIFAR10(root='../DATA/gan_dataset/training/data', train=True, download=True, transform=transform)
+    dataset = torchvision.datasets.CIFAR10(root='./DATA/gan_dataset/training/data', train=True, download=True, transform=transform)
     dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=2)
     return dataloader
 
